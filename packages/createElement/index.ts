1e5e45616667b0ece90bd3653a2593656c96444d
湫曗 <autumnsilverleaves@gmail.com>
2021-09-12 06:13:33 +0800

    feat: createElement

---
+++ packages/createElement/index.ts
@@ -0,0 +1,68 @@
+import { convertObjectToAttrStringArray, getNode, kebabCase } from './tool'
+
+type TChildren = any[]
+
+type TElementTagNameMap = HTMLElementTagNameMap
+
+type TClassType = string | { [key: string]: boolean }
+
+export function createElement<K extends keyof TElementTagNameMap>(
+  tag: K,
+  attrs: TElementTagNameMap[K],
+  childNodes: TChildren = [],
+): TElementTagNameMap[K] {
+  const element = document.createElement(tag)
+
+  Object.entries(attrs).forEach(([key, value]) => {
+    // 防止修改原生的函数
+    if (
+      typeof element[key as keyof (HTMLElement | SVGAElement)] === 'function'
+    ) {
+      console.error('incorrect key', tag)
+      throw new Error('incorrect key')
+    }
+
+    // 设置普通属性
+    else {
+      const tempValue: any = value
+      let realValue: string
+
+      // 处理 class
+      if (key === 'class' || key === 'className') {
+        realValue = [tempValue]
+          .flat(Infinity)
+          .map((item: TClassType) => {
+            return typeof item === 'object'
+              ? convertObjectToAttrStringArray(item)
+              : item
+          })
+          .flat()
+          // .map(kebabCase)
+          .join(' ')
+      }
+      // 处理 style
+      else if (key === 'style' && typeof tempValue === 'object') {
+        realValue = Object.entries(tempValue)
+          .map(([key, value]) => {
+            return `${kebabCase(key)}:${value}`
+          })
+          .join(';')
+      } else {
+        realValue = tempValue
+
+        // 设置普通属性
+        element.setAttribute(key, realValue)
+      }
+    }
+  })
+
+  // 添加子元素
+  childNodes
+    .flat(Infinity)
+    .map((child) => getNode(child))
+    .forEach((child) => {
+      element.appendChild(child)
+    })
+
+  return element as unknown as TElementTagNameMap[K]
+}
---
+++ packages/createElement/tool.ts
@@ -0,0 +1,20 @@
+export function getNode(n: any) {
+  return n instanceof Node ? n : document.createTextNode(String(n))
+}
+
+// todo: need more safe impl
+export function kebabCase(str: string) {
+  return str.replace(/([A-Z])/g, function ($1) {
+    return '-' + $1.toLocaleLowerCase()
+  })
+}
+
+export function convertObjectToAttrStringArray(o: Object) {
+  return Object.entries(o)
+    .filter(([v, should]) => {
+      return Boolean(should)
+    })
+    .map(([v]) => {
+      return v
+    })
+}
