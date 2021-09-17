ae76287e88fac3ba3bfa5485be6664c35bb58b27
湫曗 <autumnsilverleaves@gmail.com>
2021-09-17 22:57:37 +0800

    perf: simplify create logic

--- packages/tsx/src/tsx/createIntrinsicElement.ts
+++ packages/tsx/src/tsx/createIntrinsicElement.ts
@@ -16,72 +16,62 @@ export const createIntrinsicElement = <P extends {}>(
   const currentCtx = getCurrentContext()
   const shouldShow = shouldShowComponent(props)
 
-  if (currentCtx.created) {
-    const vDomNode =
-      currentCtx.domNodeInfo.list[currentCtx?.domNodeInfo.current]!
-    currentCtx!.domNodeInfo.current++
-
-    if (shouldShow) {
-      if (!vDomNode.node) {
-        vDomNode.node = createElement(tag, props as any, children)
-      } else {
-        setAttrs(vDomNode.node as HTMLElement, props)
-        // todo
-        // 设置文字节点，需要额外的处理逻辑
-        // 此处为了demo 快速跑通, 文字节点同级无node节点，这里先直接将文字设置上去
-        {
-          const stringContent = children
-            .map((child) => {
-              const type = typeof child
-              switch (type) {
-                case 'boolean':
-                case 'bigint':
-                case 'number':
-                case 'string':
-                  return child
-                  break
-
-                default:
-                  return ''
-                  break
-              }
-            })
-            .join('')
-
-          if (stringContent.length) {
-            ;(vDomNode.node as HTMLDivElement).innerText = children[0] as string
-          } else {
-            // todo
-            // 若有新加入的节点，只需要将新加入的节点插入对应位置即可
-            // 在domNode中标记出来，节点是新加入的这样就非常的容易处理了
-            // 若没有新加入的节点，则什么都不需要做
-            // 这里为了快速跑通，先直接整体替换
-            replaceChildren(vDomNode.node, children as Node[])
-          }
-        }
-      }
-
-      return vDomNode.node
-    } else {
-      if (vDomNode.node) {
-        ;(vDomNode.node as ChildNode).remove()
-        vDomNode.node = null
-      }
-      return emptyNode
-    }
-  } else {
+  if (!currentCtx.created) {
     const vDomNode = <VDomNode>{
       node: null,
     }
     currentCtx?.domNodeInfo.list.push(vDomNode)
+  }
 
-    if (shouldShow) {
-      const ele = createElement(tag, props as any, children)
+  const vDomNode = currentCtx.domNodeInfo.list[currentCtx?.domNodeInfo.current]!
+  currentCtx!.domNodeInfo.current++
 
-      vDomNode.node = ele
-      return vDomNode.node
+  if (shouldShow) {
+    if (!vDomNode.node) {
+      vDomNode.node = createElement(tag, props as any, children)
     } else {
-      return emptyNode
+      setAttrs(vDomNode.node as HTMLElement, props)
+      // todo
+      // 设置文字节点，需要额外的处理逻辑
+      // 此处为了demo 快速跑通, 文字节点同级无node节点，这里先直接将文字设置上去
+      {
+        const stringContent = children
+          .map((child) => {
+            const type = typeof child
+            switch (type) {
+              case 'boolean':
+              case 'bigint':
+              case 'number':
+              case 'string':
+                return child
+                break
+
+              default:
+                return ''
+                break
+            }
+          })
+          .join('')
+
+        if (stringContent.length) {
+          ;(vDomNode.node as HTMLDivElement).innerText = children[0] as string
+        } else {
+          // todo
+          // 若有新加入的节点，只需要将新加入的节点插入对应位置即可
+          // 在domNode中标记出来，节点是新加入的这样就非常的容易处理了
+          // 若没有新加入的节点，则什么都不需要做
+          // 这里为了快速跑通，先直接整体替换
+          replaceChildren(vDomNode.node, children as Node[])
+        }
+      }
     }
+
+    return vDomNode.node
+  } else {
+    if (vDomNode.node) {
+      ;(vDomNode.node as ChildNode).remove()
+      vDomNode.node = null
+    }
+    return emptyNode
   }
 }
