1947cf0e68f995ee5c0849d47632c3146f4e2998
湫曗 <autumnsilverleaves@gmail.com>
2021-09-25 00:05:11 +0800

    perf: use windicss for example

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -1,5 +1,5 @@
 import { mount } from '@shrio/tsx'
 import { Welcome } from './app'
-
+import 'virtual:windi.css'
 const root = document.getElementById('app')
 mount(root!, <Welcome></Welcome>)
---
+++ packages/example/vite.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite'
+import windicss from 'vite-plugin-windicss'
+export default defineConfig({
+  plugins: [windicss()],
+})
