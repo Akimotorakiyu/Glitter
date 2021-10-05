a45778c91e7f994f95dc86726102aeebab62683d
湫曗 <autumnsilverleaves@gmail.com>
2021-10-05 14:18:10 +0800

    chore: arrangeChildren

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -1,5 +1,7 @@
-import { mount } from '@shrio/tsx'
 import { Welcome } from './app'
 import 'virtual:windi.css'
-const root = document.getElementById('app')
-mount(root!, <Welcome></Welcome>)
+// const root = document.getElementById('app')
+import { arrangeChildren, getNode } from '@shrio/tsx'
+
+// mount(root!, <Welcome></Welcome>)
+arrangeChildren(document.body, [getNode(<Welcome></Welcome>)])
