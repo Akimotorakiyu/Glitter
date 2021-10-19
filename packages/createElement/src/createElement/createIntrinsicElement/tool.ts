0014a0a20bf62a70a9398460849ebc58280206d3
湫曗 <autumnsilverleaves@gmail.com>
2021-10-19 22:20:59 +0800

    feat: add propsChanged & updateChildNodes

--- packages/createElement/src/createElement/createIntrinsicElement/tool.ts
+++ packages/createElement/src/createElement/createIntrinsicElement/tool.ts
@@ -14,27 +14,46 @@ export const updateProps = (
   newProps: Record<string, any>,
 ) => {
   const keys = Object.keys(newProps)
+  let propsChanged = false
   Object.keys(targetProps).forEach((key) => {
     if (!keys.includes(key)) {
       delete targetProps[key]
+      propsChanged = true
     }
   })
 
   Object.entries(newProps).forEach(([key, value]) => {
     if (targetProps[key] !== value) {
       targetProps[key] = value
+      propsChanged = true
     }
   })
+
+  return propsChanged
 }
 
 export const updateChildNodes = (
   targetChildNodes: Node[],
   newChildNodes: Node[],
 ) => {
-  targetChildNodes.length = 0
-  newChildNodes.forEach((child) => {
-    targetChildNodes.push(child)
-  })
+  let childrenChanged = false
+
+  if (targetChildNodes.length !== newChildNodes.length) {
+    childrenChanged = true
+  } else {
+    const hasChange = newChildNodes.some((child, index) => {
+      const isTheSame = child !== targetChildNodes[index]
+      return isTheSame
+    })
+    childrenChanged = hasChange
+  }
+
+  if (childrenChanged) {
+    targetChildNodes.length = 0
+    newChildNodes.forEach((child) => {
+      targetChildNodes.push(child)
+    })
+  }
 }
 
 export const shouldShowComponent = <P>(props: P) => {
