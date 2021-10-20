bab9ad9393b92faa29bc3bc26089fcfdbd8d25e1
湫曗 <autumnsilverleaves@gmail.com>
2021-10-20 22:31:51 +0800

    chore: use shalllow mode

--- packages/shrio/src/compositionApi/shrioReactive.ts
+++ packages/shrio/src/compositionApi/shrioReactive.ts
@@ -1,6 +1,6 @@
-import { useAsyncUpdater } from '@shiro/create-element'
+import { useAsyncUpdater, setUpdateMode } from '@shiro/create-element'
 import { reactive, TKey } from '@shiro/reactive'
-
+setUpdateMode('shallow')
 export const shrioReactive = <T extends Record<TKey, any>>(target: T) => {
   return reactive(target, (t, k) => {
     const asyncUpdater = useAsyncUpdater()
