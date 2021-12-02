e084ba9fd718c1384226b1d9096c95daf8624d3e
湫曗 <autumnsilverleaves@gmail.com>
2021-12-02 20:24:28 +0800

    feat: defineView support customs

--- packages/createElement/src/defineComponent/defineView.ts
+++ packages/createElement/src/defineComponent/defineView.ts
@@ -1,4 +1,7 @@
-import { IFunctionComponent } from '../createElement/createComponent/componentContext/type'
+import {
+  IFunctionComponent,
+  Context,
+} from '../createElement/createComponent/componentContext/type'
 import { ShrioProps } from './type'
 
 /**
@@ -6,8 +9,25 @@ import { ShrioProps } from './type'
  * @param view
  * @returns
  */
-export const defineView = <P extends Record<string, unknown>>(
+export const defineView = <P extends {}>(
   view: IFunctionComponent<P>,
-): IFunctionComponent<P & ShrioProps> => {
-  return view
+  customs?:
+    | Partial<P & ShrioProps>
+    | ((
+        p: P & ShrioProps & Record<string, unknown>,
+        children: Node[],
+        ctx: Context,
+      ) => Partial<P & ShrioProps>),
+): IFunctionComponent<P & ShrioProps & Record<string, unknown>> => {
+  if (customs) {
+    return defineView<P & ShrioProps & Record<string, unknown>>((...args) => {
+      args[0] = {
+        ...(typeof customs === 'function' ? customs(...args) : customs),
+        ...args[0],
+      }
+      return view(...args)
+    })
+  } else {
+    return view
+  }
 }
