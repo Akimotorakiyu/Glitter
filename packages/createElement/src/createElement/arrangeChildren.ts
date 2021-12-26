571f7a037f02df80a516f49c2f6860e3eda6470c
湫曗 <autumnsilverleaves@gmail.com>
2021-12-26 14:47:23 +0800

    feat: arrangeChildren

--- packages/createElement/src/createElement/arrangeChildren.ts
+++ packages/createElement/src/createElement/arrangeChildren.ts
@@ -6,5 +6,29 @@ export const arrangeChildren = (
   childNodes: Node[],
 ) => {
   generateAndApplyReMounter(parentElement, childNodes)
-  parentElement.replaceChildren(...childNodes)
+
+  const flatedChildNodes = childNodes.reduce((acc, child) => {
+    if (child instanceof DocumentFragment) {
+      return acc.concat(...child.childNodes)
+    } else {
+      acc.push(child)
+    }
+    return acc
+  }, [] as Node[])
+
+  const childNodesSet = new Set(flatedChildNodes)
+
+  for (let index = 0; parentElement.childNodes.item(index); index++) {
+    const element = parentElement.childNodes.item(index)
+    if (!childNodesSet.has(element)) {
+      element.remove()
+    }
+  }
+
+  flatedChildNodes.forEach((child, index) => {
+    const nIndexChildInParent = parentElement.childNodes.item(index)
+    if (!(nIndexChildInParent === child)) {
+      parentElement.insertBefore(child, nIndexChildInParent)
+    }
+  })
 }
