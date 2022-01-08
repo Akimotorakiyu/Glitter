7ff25d979b9b2b6beeefe54ad86b96481746c223
湫曗 <autumnsilverleaves@gmail.com>
2022-01-08 22:24:28 +0800

    perf: 使用原型链的办法，可以不用介意 state 上也有一个 render

--- packages/createElement/src/defineComponent/defineFactory.ts
+++ packages/createElement/src/defineComponent/defineFactory.ts
@@ -14,12 +14,15 @@ export const defineFactoryComponent = <
   const factory = (props: any, children: any, context: any) => {
     const _state = stateFactory(props, children, context)
     if (typeof _state !== 'object') {
-      throw new Error('render should be an object')
+      throw new Error('state should be an object')
     }
-    if (_state && 'render' in _state) {
-      throw new Error('render should not in state')
-    }
-    const com = Object.assign(_state, {
+
+    // 使用原型链的办法，可以不用介意 state 上也有一个 render
+    // if (_state && 'render' in _state) {
+    //   throw new Error('render should not in state')
+    // }
+
+    const com = Object.assign(Object.create(_state), {
       render: () => {
         return view(_state, children, context)
       },
