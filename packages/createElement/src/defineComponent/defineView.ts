8a318912e683d687f98e9060c5c3011d1ab79648
湫曗 <autumnsilverleaves@gmail.com>
2022-01-15 14:25:04 +0800

    chore: remove custom from define view

--- packages/createElement/src/defineComponent/defineView.ts
+++ packages/createElement/src/defineComponent/defineView.ts
@@ -12,23 +12,6 @@ import { ShrioProps } from './type'
  */
 export const defineView = <P extends {}>(
   view: IFunctionComponent<P>,
-  customs?:
-    | Partial<P & ShrioProps>
-    | ((
-        p: P & ShrioProps & Record<string, unknown>,
-        children: (Node | ShrioFragment)[],
-        ctx: Context,
-      ) => Partial<P & ShrioProps>),
 ): IFunctionComponent<P & ShrioProps & Record<string, unknown>> => {
-  if (customs) {
-    return defineView<P & ShrioProps & Record<string, unknown>>((...args) => {
-      args[0] = {
-        ...(typeof customs === 'function' ? customs(...args) : customs),
-        ...args[0],
-      }
-      return view(...args)
-    })
-  } else {
-    return view
-  }
+  return view
 }
