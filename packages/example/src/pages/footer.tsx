34cdcd5c2d4ade54322088c1a9dce40da357d469
湫曗 <autumnsilverleaves@gmail.com>
2024-12-23 01:03:02 +0800

    feat: up Copyright

--- packages/example/src/pages/footer.tsx
+++ packages/example/src/pages/footer.tsx
@@ -16,7 +16,7 @@ export const Footer = defineView(() => {
           <span class="text-sm text-gray-600">Power by Gillter</span>
         </p>
         <p>
-          <span class="text-sm text-gray-600">Copyright © 2021-2021 湫曗</span>
+          <span class="text-sm text-gray-600">Copyright © 2021-2025 湫曗</span>
         </p>
       </div>
     </div>
